10ee8983424e02d1cfb10204babe27ac91573124
user <hellios.dt@gmail.com>
2018-03-01 23:05:46 +0200

    start

--- src/Manager.java
+++ src/Manager.java
@@ -1,5 +1,10 @@
-public class Manager {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+public class Manager {
+    List<String> lines = new ArrayList<>();
     private Road currentRoad;
     private City city;
 
@@ -8,14 +13,17 @@ public class Manager {
     }
 
 
+
     public void start(){
 
-        
+
 
     }
 
 
-
+    public void out{
+        city.getVehicles().forEach(i -> new String(city.getVehicles().indexOf(i) +" " + i.getRoads().stream().map(i -> i.getIdRoad()).collect(Collectors.joining(" "))));
+    }
 
 
 }
