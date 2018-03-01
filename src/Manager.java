99237e1bc2a97bcc566bae84b78b520d577e2224
kicha <ivankika.1999@gmail.com>
2018-03-01 23:24:25 +0200

    Created class City

--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Manager {
 
     private Road currentRoad;
@@ -8,8 +10,16 @@ public class Manager {
     }
 
 
-    public void start(){
-
+    public void choseCar(ArrayList<Vehicle> vehicles, Road road){
+        ArrayList<Vehicle> cars = new ArrayList<>();
+        for (Vehicle vehicle : vehicles ) {
+            int way = Math.abs(vehicle.getRow() - road.getStartRow()) + Math.abs(vehicle.getColumn()-road.getStartColumn()) +
+                    Math.abs(road.getStartColumn() - road.getEndColumn()) + Math.abs(road.getStartRow() - road.getEndRow());
+            if (way < vehicle.getTtl() && way < road.getEndTime() - road.getStartTime())
+                if (vehicle.getRoads().size() > 0)
+                    if (!(vehicle.getRoads().get(vehicle.getRoads().size()-1).getEndTime() < road.getStartTime()))
+                        
+        }
         
 
     }
