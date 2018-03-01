90269e1ab5cc243b7acf4ef21991203da40a58a6
kicha <ivankika.1999@gmail.com>
2018-03-01 21:41:15 +0200

    Created class City

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,15 +1,37 @@
 import java.util.ArrayList;
 
 public class Vehicle {
+    private int row;
+    private int column;
+    private int ttl;
     private ArrayList<Road> roads;
 
-    public Vehicle(){
+    public Vehicle(int steps){
+        row = 0;
+        column = 0;
+        ttl = steps;
+        roads = new ArrayList<Road>();
 
     }
 
 
-    public ArrayList<Road> getRoads() {
+    public int getRow() {
+        return row;
+    }
 
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public ArrayList<Road> getRoads() {
         return roads;
     }
 
