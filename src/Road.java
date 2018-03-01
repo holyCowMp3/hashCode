38e6102ef26c3e038228c6f81e59bbe37896dd7d
user <hellios.dt@gmail.com>
2018-03-01 21:21:58 +0200

    start

--- src/Road.java
+++ src/Road.java
@@ -1,2 +1,25 @@
+import javafx.beans.property.StringProperty;
+
 public class Road {
+
+   private int startRow;
+   private int startColumn;
+   private int endRow;
+   private int endColumn;
+   private int start
+   private int roadLength;
+
+    public Road(String input) {
+        String[] strings = input.split(" ");
+        this.startRow = startRow;
+        this.startColumn = startColumn;
+        this.endRow = endRow;
+        this.endColumn = endColumn;
+        roadLength.
+    }
+
+    Road(){
+
+    }
+
 }
